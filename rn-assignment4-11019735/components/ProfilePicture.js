29796ac8632bf5b4417485c38835fea53e2e2543
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 04:00:35 -0700

    positioned the ellipse icon on the profile picture

--- rn-assignment4-11019735/components/ProfilePicture.js
+++ rn-assignment4-11019735/components/ProfilePicture.js
@@ -1,19 +1,39 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 export default function ProfilePicture() {
   return (
-    <Image
-      style={styles.image}
-      source={require("../assets/ProfilePicture.png")}
-    />
+    <View style={styles.profilePictureContainer}>
+      <Image
+        style={styles.image}
+        source={require("../assets/ProfilePicture.png")}
+      />
+      <Image
+        style={styles.ellipse}
+        source={require("../assets/Ellipse 767.png")}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  profilePictureContainer: {
+    width: 54,
+    height: 54,
+    left: 30,
+    position: "relative",
+  },
   image: {
     width: 54,
     height: 54,
     borderRadius: 27,
     left: 30,
+  },
+  ellipse: {
+    backgroundColor: "white",
+    width: 8,
+    height: 8,
+    position: "absolute",
+    left: 69,
+    top: 6,
   },
 });
