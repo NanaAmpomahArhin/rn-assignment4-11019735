62d64eef083b7cd206589a5d79c1172c3b882d12
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 16:51:38 -0700

    created data for icons

---
+++ rn-assignment4-11019735/Data/IconData.js
@@ -0,0 +1,11 @@
+export const iconData = [
+  {
+    id: 0,
+    image: require("./../assets/cib_apple.png"),
+  },
+  {
+    id: 1,
+    image: require("./../assets/google.png"),
+  },
+  { id: 2, image: require("./../assets/facebook.png") },
+];
