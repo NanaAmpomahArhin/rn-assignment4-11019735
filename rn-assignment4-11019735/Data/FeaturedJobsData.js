48b70e9a65fa77ee58574d8b1b41731b69bacdde
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 00:59:56 -0700

    created a directory to store data

---
+++ rn-assignment4-11019735/Data/FeaturedJobsData.js
@@ -0,0 +1,18 @@
+export const featuredJobs = [
+  {
+    id: 0,
+    role: "Software Engineer",
+    organization: "Facebook",
+    location: "Accra,Ghana",
+    salary: "$180,00",
+    image: require("./../assets/facebook.png"),
+  },
+  {
+    id: 1,
+    role: "Web Developer",
+    organization: "Google",
+    location: "Accra,Ghana",
+    salary: "$160,00",
+    image: require("./../assets/google.png"),
+  },
+];
---
+++ rn-assignment4-11019735/Data/PopularJobsData.js
@@ -0,0 +1,26 @@
+export const popularJobs = [{
+    id:0,
+    role:"Jr Executive",
+    organization:"Burger King",
+    location:"Los Angeles,US",
+    salary:"$96,000/y",
+    image:require("../../assets/burgerking.png")
+},
+{
+id :1,
+role:"Product Manager",
+organization:"Beats",
+location:"Florida,US",
+salary:"$84,000/y",
+image:require("../../assets/beats.png")
+},
+{
+    id :2,
+    role:"Product Manager",
+    organization:"Facebook",
+    location:"Florida,US",
+    salary:'$86,000/y',
+    image:require("../../assets/facebook.png")
+}
+
+]
