b159c198266336fc0c0ae6e392092bd6faba401c
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 01:08:43 -0700

    used the JobsContainer component

--- rn-assignment4-11019735/Screens/HomeScreen.js
+++ rn-assignment4-11019735/Screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import { View, StyleSheet } from "react-native";
 import ProfileContainer from "../components/ProfileContainer";
 import SearchContainer from "../components/SearchContainer";
+import JobsContainer from "../components/JobsContainer";
+import { featuredJobs } from "../Data/FeaturedJobsData";
+import { featuredJobsStyles } from "../Styles/FeaturedJobsStyles";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ProfileContainer />
       <SearchContainer />
+      <JobsContainer data={featuredJobs} styles={featuredJobsStyles} />
     </View>
   );
 }
