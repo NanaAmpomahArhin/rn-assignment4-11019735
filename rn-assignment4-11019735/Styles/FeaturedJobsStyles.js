0cfec05ead617a21aa45f0e6c857adc15a574cda
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 01:00:39 -0700

    created a directory to store stylesheets

---
+++ rn-assignment4-11019735/Styles/FeaturedJobsStyles.js
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+export const featuredJobsStyles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 400,
+    top: 180,
+    left: 80,
+  },
+  cardWrapper: {
+    width: 280,
+    height: 186,
+    borderRadius: 24,
+    backgroundColor: "#34A853",
+    marginRight: 15,
+    position: "relative",
+  },
+  imageAndRoleContainer: {
+    flexDirection: "row",
+    width: 207,
+    height: 47,
+    position: "absolute",
+    top: 15,
+    left: 28,
+  },
+  imageContainer: {
+    width: 46,
+    height: 46,
+    borderRadius: 12,
+    backgroundColor: "white",
+    position: "relative",
+    marginRight: 20,
+  },
+  image: {
+    width: 22,
+    height: 23.57,
+    position: "absolute",
+    top: 10,
+    left: 10,
+  },
+  roleAndOrganizationContainer: {
+    width: 145,
+    height: 46,
+    flexDirection: "column",
+  },
+  role: {
+    fontFamiliy: "Poppins",
+    fontWeight: 600,
+    fontSize: 16,
+    lineHeight: 20.8,
+    color: "white",
+  },
+  organization: {
+    fontFamily: "Poppins",
+    fontWeight: 400,
+    fontSize: 14,
+    color: "white",
+    opacity: 0.75,
+  },
+  salaryAndLocationContainer: {
+    flexDirection: "row",
+    width: 236,
+    height: 26,
+    justifyContent: "space-between",
+    position: "absolute",
+    bottom: 15,
+    left: 20,
+  },
+  salary: {
+    font: "Poppins",
+    fontWeight: 500,
+    fontSize: 15,
+    color: "white",
+  },
+  location: {
+    font: "Poppins",
+    fontWeight: 500,
+    fontSize: 15,
+    color: "white",
+  },
+});
