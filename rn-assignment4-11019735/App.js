52f667c991ccb15f3719fe28cea870957d4e102f
anarhin002@st.ug.edu.gh <anarhin002@st.ug.edu.gh>
2024-06-15 17:10:53 -0700

    used the bottom tab navigator to navigate between screens

--- rn-assignment4-11019735/App.js
+++ rn-assignment4-11019735/App.js
@@ -1,9 +1,17 @@
-import { View } from "react-native";
-import ProfileContainer from "./components/ProfileContainer";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./Screens/HomeScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View>
-      <ProfileContainer />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Login" component={LoginScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
